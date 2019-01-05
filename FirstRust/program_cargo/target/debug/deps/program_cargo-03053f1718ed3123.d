ad781d2fd298b31d2f28e778255137e5fc1cc6c6
Paromita Datta <paromitadatta@Paromitas-MacBook-Pro.local>
2019-01-05 11:13:02 -0600

    Trying to Make the Files Accessible

---
+++ FirstRust/program_cargo/target/debug/deps/program_cargo-03053f1718ed3123.d
@@ -0,0 +1,5 @@
+/Users/paromitadatta/Desktop/projects/program_cargo/target/debug/deps/program_cargo-03053f1718ed3123: src/main.rs
+
+/Users/paromitadatta/Desktop/projects/program_cargo/target/debug/deps/program_cargo-03053f1718ed3123.d: src/main.rs
+
+src/main.rs:
